38fbc72f5ea21b5782dfb758b0b1a883df4efa69
Chang <c.li1@lumc.nl>
2025-06-26 14:31:43 +0200

    Update plugin config file

--- src/GeneSurferPlugin.h
+++ src/GeneSurferPlugin.h
@@ -338,7 +338,7 @@ class GeneSurferPluginFactory : public ViewPluginFactory
     Q_INTERFACES(mv::plugin::ViewPluginFactory mv::plugin::PluginFactory)
     Q_OBJECT
     Q_PLUGIN_METADATA(IID   "nl.BioVault.GeneSurferPlugin"
-                      FILE  "GeneSurferPlugin.json")
+                      FILE  "PluginInfo.json")
 
 public:
 
